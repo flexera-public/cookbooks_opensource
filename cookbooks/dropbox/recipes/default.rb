b425ad2bd9dfe49fb6e194323108f35bce129b53
caryp <cary@rightscale.com>
2010-06-08 21:47:52 -0700

    remove hardcoded home dir

--- cookbooks/dropbox/recipes/default.rb
+++ cookbooks/dropbox/recipes/default.rb
@@ -3,26 +3,29 @@
 # Recipe:: default
 #
 
-OUTPUT_FILE = "dropbox.log"
-DROPBOX_EXEC = "/root/.dropbox-dist/dropboxd"
+user = node.etc.passwd[node.current_user]
+raise "ERROR: valid user #{user} not found on system." unless user
+home_dir = user.dir
+raise "ERROR: user #{user} must have a valid home dir." unless home_dir
 
-platform = node[:kernel][:machine]
+OUTPUT_FILE = "dropbox.log"
+DROPBOX_EXEC = "#{home_dir}/.dropbox-dist/dropboxd"
+
+platform = node.kernel.machine
 suffix = (platform == "x86_64") ? platform : "x86"
 
 bash "download dropbox" do
   not_if do ::File.exists?(DROPBOX_EXEC) end
-  cwd "/root"
+  cwd home_dir
   code <<-EOH
     wget -O dropbox.tar.gz http://www.getdropbox.com/download?plat=lnx.#{suffix}
-    wget http://dl.getdropbox.com/u/6995/dbmakefakelib.py
-    wget http://dl.getdropbox.com/u/6995/dbreadconfig.py
     tar zxof dropbox.tar.gz
   EOH
 end
 
 bash "download CLI tool" do
   not_if do ::File.exists?("/usr/local/bin/dropbox.py") end
-  cwd "/root"
+  cwd home_dir
   code <<-EOH
     wget -P /usr/local/bin http://www.dropbox.com/download?dl=packages/dropbox.py
     mv /usr/local/bin/dropbox.py /usr/local/bin/dropbox.py
@@ -38,7 +41,7 @@ ruby_block "check download" do
   end
 end
 
-template "/root/.dropbox-dist/dropbox.sh" do
+template "#{home_dir}/.dropbox-dist/dropbox.sh" do
   source "dropbox.sh.erb"
   mode "770"
 end
@@ -61,17 +64,17 @@ ruby_block "wait for log file" do
   block do
     Chef::Log.info "Waiting for logfile to exist.."
     60.times do
-      break if ::File.exists?("/root/#{OUTPUT_FILE}")
+      break if ::File.exists?("#{home_dir}/#{OUTPUT_FILE}")
       Chef::Log.info "  retrying..."
       sleep 5
     end
-    raise "Dropbox logfile not found. Unable to register instance!. Fail." unless ::File.exists?("/root/#{OUTPUT_FILE}")
+    raise "Dropbox logfile not found. Unable to register instance!. Fail." unless ::File.exists?("#{home_dir}/#{OUTPUT_FILE}")
   end
 end
 
 ruby_block "register instance" do
-  only_if do ::File.exists?("/root/#{OUTPUT_FILE}") end
-  not_if do ::File.directory?("/root/Dropbox") end
+  only_if do ::File.exists?("#{home_dir}/#{OUTPUT_FILE}") end
+  not_if do ::File.directory?("#{home_dir}/Dropbox") end
   block do
     # wait for the log to catchup and have the registration link
     sleep 10
@@ -83,13 +86,13 @@ ruby_block "register instance" do
     data << "-d remember_me=on "
     data << "-d t=791206fc33 "
 
-    link_line = `grep "link this machine" /root/#{OUTPUT_FILE}`
+    link_line = `grep "link this machine" #{home_dir}/#{OUTPUT_FILE}`
     words = link_line.split
     url = words[2]
     data << "-d cont=#{url}"
     
     Chef::Log.info "Registering instance using URL: #{url}"
-    cmd = "curl -L -c cookies.txt #{data} -o /root/dropbox_register.log --url https://www.dropbox.com/login"
+    cmd = "curl -L -c cookies.txt #{data} -o #{home_dir}/dropbox_register.log --url https://www.dropbox.com/login"
     Chef::Log.info "Running command: #{cmd}"
     Kernel.system(cmd)
   end
