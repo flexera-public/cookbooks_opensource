54837a50e3be8360f6510caed9cfe86823e0c1ac
caryp <cary@rightscale.com>
2010-06-11 10:59:19 -0700

    Misc. cleanup

--- cookbooks/mongodb/metadata.rb
+++ cookbooks/mongodb/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "RightScale opensource"
 maintainer_email "opensource@rightscale.com"
-license          "Apache 2.0"
+license          IO.read(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE'))
 description      "Installs/Configures mongodb"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
--- cookbooks/phpmyadmin/metadata.rb
+++ cookbooks/phpmyadmin/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Example Com"
 maintainer_email "ops@example.com"
-license          "Apache 2.0"
+license          IO.read(File.join(File.dirname(__FILE__), '..', '..', 'LICENSE'))
 description      "Installs/Configures phpmyadmin"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.1"
